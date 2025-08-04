3f0a8156a0c19259da8726be7486aa6dfc7a5a06
Diogo Silva <dbtdsilva@gmail.com>
2025-08-04 14:00:34 +0200

    feat: added configuration for which usb is listen when changing displays

--- src/plugins/device_display_mapper/device_display_mapper_plugin.py
+++ src/plugins/device_display_mapper/device_display_mapper_plugin.py
@@ -6,7 +6,7 @@ from PySide6.QtWidgets import QMenu, QWidget
 from PySide6.QtCore import Slot
 
 from ...base.user_settings import UserSettings
-from .device_listener import DeviceListener
+from .device_listener import DeviceListener, DeviceNotificationType
 from ...base.base_widget import BaseWidget
 
 import monitorcontrol
@@ -19,13 +19,13 @@ class DeviceDisplayMapperPlugin(BaseWidget):
         super().__init__(parent)
 
         self.user_settings = UserSettings.instance()
-        if not self.user_settings.has_key('input_on_disconnect'):
-            self.user_settings.set('input_on_disconnect', monitorcontrol.InputSource.HDMI1)
-        if not self.user_settings.has_key('input_on_connect'):
-            self.user_settings.set('input_on_connect', monitorcontrol.InputSource.DP1)
+        if not self.user_settings.has_key('display_on_disconnect'):
+            self.user_settings.set('display_on_disconnect', monitorcontrol.InputSource.HDMI1)
+        if not self.user_settings.has_key('display_on_connect'):
+            self.user_settings.set('display_on_connect', monitorcontrol.InputSource.DP1)
 
-        logging.info(f"Starting with the 'input_on_connect' set to {self.user_settings.get('input_on_connect')}")
-        logging.info(f"Starting with the 'input_on_disconnect' set to {self.user_settings.get('input_on_disconnect')}")
+        logging.info(f"Starting with the 'display_on_connect' set to {self.user_settings.get('display_on_connect')}")
+        logging.info(f"Starting with the 'display_on_disconnect' set to {self.user_settings.get('display_on_disconnect')}")
 
         self.last_process = 0
         self.device_listener = DeviceListener(self)
@@ -33,14 +33,32 @@ class DeviceDisplayMapperPlugin(BaseWidget):
 
     def retrieve_menus(self) -> list[QMenu]:
         return [
-            self.createInputMenu('Input on connect',
-                                 self.change_input_source_on_connect,
-                                 'input_on_connect'),
-            self.createInputMenu('Input on disconnect',
-                                 self.change_input_source_on_disconnect,
-                                 'input_on_disconnect')]
+            self.create_display_selection_menu('Display on connect',
+                                               self.change_display_on_input_connect,
+                                 'display_on_connect'),
+            self.create_display_selection_menu('Display on disconnect',
+                                               self.change_display_on_input_disconnect,
+                                 'display_on_disconnect'),
+            self.create_usb_selection_menu('USB to be watched for display connection',
+                                           self.change_usb_watcher,
+                                           'usb_watcher'),
+        ]
 
-    def createInputMenu(self, title, change_value_trigger, key):
+    def create_usb_selection_menu(self, title, change_value_trigger, key):
+        menu = QMenu(title, self)
+        group = QActionGroup(self)
+        group.setExclusive(True)
+        for source in self.device_listener.get_real_usb_devices():
+            action = QAction(str(source), self)
+            action.setCheckable(True)
+            action.triggered.connect(partial(lambda val: change_value_trigger(val), val=source))
+            if source == self.user_settings.get(key):
+                action.setChecked(True)
+            group.addAction(action)
+            menu.addAction(action)
+        return menu
+
+    def create_display_selection_menu(self, title, change_value_trigger, key):
         menu = QMenu(title, self)
         group = QActionGroup(self)
         group.setExclusive(True)
@@ -54,27 +72,33 @@ class DeviceDisplayMapperPlugin(BaseWidget):
             menu.addAction(action)
         return menu
 
-    def change_input_source_on_connect(self, source):
-        self.user_settings.set('input_on_connect', source)
+    def change_display_on_input_connect(self, source):
+        self.user_settings.set('display_on_connect', source)
 
-    def change_input_source_on_disconnect(self, source):
-        self.user_settings.set('input_on_disconnect', source)
+    def change_display_on_input_disconnect(self, source):
+        self.user_settings.set('display_on_disconnect', source)
+
+    def change_usb_watcher(self, source):
+        self.user_settings.set('usb_watcher', source)
 
     @Slot(bool, str)
-    def device_changed(self, connected, usb):
+    def device_changed(self, device_notification_type: DeviceNotificationType, usb_device_id: str):
         current_time = time.time()
         if current_time - self.last_process < 1.0:
+            return
+
+        if self.user_settings.get('usb_watcher') != usb_device_id:
             return
 
         self.last_process = time.time()
         for i, monitor in enumerate(monitorcontrol.get_monitors()):
             with monitor:
-                if connected:
-                    input_source = self.user_settings.get('input_on_connect')
+                if device_notification_type == DeviceNotificationType.CREATION:
+                    input_source = self.user_settings.get('display_on_connect')
                     monitor.set_input_source(input_source)  # type: ignore
                     logging.info(f"Changing monitor {i} input source to {input_source}")
-                else:
-                    input_source = self.user_settings.get('input_on_disconnect')
+                elif device_notification_type == DeviceNotificationType.DELETION:
+                    input_source = self.user_settings.get('display_on_disconnect')
                     monitor.set_input_source(input_source)  # type: ignore
                     logging.info(f"Changing monitor {i} input source to {input_source}")
 
--- src/plugins/device_display_mapper/device_listener.py
+++ src/plugins/device_display_mapper/device_listener.py
@@ -1,28 +1,78 @@
+from enum import StrEnum
+
 from PySide6 import QtCore
 from PySide6.QtWidgets import QWidget
 import pythoncom
 import wmi
+import re
+import logging
 
 from ...base.base_widget import BaseWidget
 
+class DeviceNotificationType(StrEnum):
+    DELETION = "Deletion"
+    CREATION = "Creation"
+    MODIFICATION = "Modification"
+    OPERATION = "Operation"
 
 class DeviceListener(BaseWidget):
 
-    change_detected = QtCore.Signal(bool, str)
+    change_detected = QtCore.Signal(DeviceNotificationType, str)
 
     def __init__(self, parent: QWidget) -> None:
         super().__init__(parent)
-        self.connect_listener = _DeviceConnectListener(parent=self)
+        logging.info('Starting device listener..')
+        self.connect_listener = _DeviceListenerThread(parent=self,
+                                                     parent_signal=self.change_detected,
+                                                      notification_type=DeviceNotificationType.CREATION)
+        self.disconnect_listener = _DeviceListenerThread(parent=self,
+                                                         parent_signal=self.change_detected,
+                                                         notification_type=DeviceNotificationType.DELETION)
         self.connect_listener.start()
-        self.disconnect_listener = _DeviceDisconnectListener(parent=self)
         self.disconnect_listener.start()
 
-        self.disconnect_listener.disconnect_signal.connect(lambda usb: self.device_change(False, usb))
-        self.connect_listener.connect_signal.connect(lambda usb: self.device_change(True, usb))
+    @staticmethod
+    def get_usb_list():
+        c = wmi.WMI()
+        usb_devices = []
 
-    @QtCore.Slot(str)
-    def device_change(self, connected, usb):
-        self.change_detected.emit(connected, usb)
+        for usb in c.query("SELECT * FROM Win32_USBControllerDevice"):
+            try:
+                dependent = usb.Dependent
+                device_info = {
+                    "DeviceID": dependent.DeviceID,
+                    "PNPDeviceID": dependent.PNPDeviceID,
+                    "Description": dependent.Description,
+                    "Name": dependent.Name
+                }
+                print(dependent)
+                usb_devices.append(device_info)
+            except Exception as e:
+                print("Failed to get USB device info:", e)
+        return usb_devices
+
+    @staticmethod
+    def is_real_usb_device(pnp_id):
+        if not pnp_id.startswith("USB\\"):
+            return False
+        if "ROOT" in pnp_id:
+            return False
+        if not re.search(r'VID_[0-9A-F]{4}&PID_[0-9A-F]{4}', pnp_id, re.IGNORECASE):
+            return False
+        return True
+
+    @staticmethod
+    def get_real_usb_devices():
+        c = wmi.WMI()
+        usb_devices = []
+
+        for device in c.Win32_PnPEntity():
+            pnp_id = getattr(device, "PNPDeviceID", "")
+            if DeviceListener.is_real_usb_device(pnp_id):
+                usb_devices.append(pnp_id)
+                print(pnp_id, device.Name, device.Description, device.Manufacturer)
+
+        return usb_devices
 
     def closeEvent(self, event):
         self.connect_listener.requestInterruption()
@@ -33,40 +83,29 @@ class DeviceListener(BaseWidget):
         event.accept()
 
 
-class _DeviceDisconnectListener(QtCore.QThread):
+class _DeviceListenerThread(QtCore.QThread):
 
-    disconnect_signal = QtCore.Signal(str)
+    def __init__(self, parent,
+                 parent_signal: QtCore.Signal(DeviceNotificationType, str),
+                 notification_type: DeviceNotificationType):
+        super().__init__(parent)
+        self.notification_type = notification_type
+        self.signal = parent_signal
 
     def run(self):
+
+        logging.info(f"Starting DeviceDisconnectListener for {self.notification_type}")
+
         pythoncom.CoInitialize()
         c = wmi.WMI()
         watcher = c.watch_for(
-            notification_type="Deletion",
-            wmi_class="Win32_PointingDevice")
-        while not self.isInterruptionRequested():
-            try:
-                usb = watcher(500)
-                self.disconnect_signal.emit(str(usb))
-            except wmi.x_wmi_timed_out:
-                pass
-        pythoncom.CoUninitialize()
-
-
-class _DeviceConnectListener(QtCore.QThread):
-
-    connect_signal = QtCore.Signal(str)
-
-    def run(self):
-        pythoncom.CoInitialize()
-        c = wmi.WMI()
-        watcher = c.watch_for(
-            notification_type="Creation",
-            wmi_class="Win32_PointingDevice",
+            notification_type=self.notification_type.value,
+            wmi_class="Win32_PnPEntity",
             delay_secs=1)
         while not self.isInterruptionRequested():
             try:
                 usb = watcher(500)
-                self.connect_signal.emit((str(usb)))
+                self.signal.emit(self.notification_type, usb.wmi_property('DeviceID').value)
             except wmi.x_wmi_timed_out:
                 pass
         pythoncom.CoUninitialize()
