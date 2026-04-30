be142359b6e54412cc782b45d92d6ec378d69c6c
Diogo Silva <dbtdsilva@gmail.com>
2026-04-30 09:40:59 +0200

    feat: move display automation from tray menu to Configuration

--- src/plugins/device_display_mapper/device_display_mapper_plugin.py
+++ src/plugins/device_display_mapper/device_display_mapper_plugin.py
@@ -1,13 +1,13 @@
 import logging
 import time
-from functools import partial
 
 import monitorcontrol
 from PySide6.QtCore import Slot
-from PySide6.QtGui import QAction, QActionGroup
+from PySide6.QtGui import QAction
 from PySide6.QtWidgets import QMenu, QWidget
 
 from ...base.base_widget import BaseWidget
+from ...base.config_panel import ConfigPanel
 from ...base.monitor_runner import runner
 from ...base.user_settings import UserSettings
 from .device_listener import Device, DeviceListener, DeviceNotificationType
@@ -38,64 +38,11 @@ class DeviceDisplayMapperPlugin(BaseWidget):
         self.device_listener.change_detected.connect(self.device_changed)
 
     def retrieve_menus(self) -> list[QMenu | QAction]:
-        menu = QMenu('Display automation', self)
+        return []
 
-        sub_menus = []
-        for monitor in monitorcontrol.get_monitors():
-            with monitor:
-                monitor_info = self.monitor_info_ctx.get_monitor_info_by_monitor(monitor=monitor)
-                if monitor_info is None:
-                    logging.warning('No monitor info not available')
-                    continue
-                monitor_label = f'{monitor_info.model} ({monitor_info.device_name})'
-                sub_menus.extend([
-                    self.create_display_selection_menu(
-                        f'Input for {monitor_label} on connect',
-                        monitor_info.inputs,
-                        USER_SETTINGS_DISPLAY_ON_CONNECT_KEY_FUNC(monitor_info.device_id)),
-                    self.create_display_selection_menu(
-                        f'Input for {monitor_label} on disconnect',
-                        monitor_info.inputs,
-                        USER_SETTINGS_DISPLAY_ON_DISCONNECT_KEY_FUNC(monitor_info.device_id))])
-        sub_menus.append(self.create_usb_selection_menu())
-        for sub_menu in sub_menus:
-            menu.addMenu(sub_menu)
-        return [menu]
-
-    def create_usb_selection_menu(self):
-        menu = QMenu('USB trigger for display switch', self)
-        group = QActionGroup(self)
-        group.setExclusive(True)
-        for device in self.device_listener.get_real_usb_devices():
-            action = QAction(f'{device.name}, {device.description} ({device.id})', self)
-            action.setCheckable(True)
-            action.setData(device)
-            action.triggered.connect(partial(lambda val: self.change_usb_watcher(val), val=device))
-            if device.id == self.user_settings.get(USER_SETTINGS_DISPLAY_USB_WATCHER_KEY):
-                action.setChecked(True)
-            group.addAction(action)
-            menu.addAction(action)
-        return menu
-
-    def create_display_selection_menu(self, title, monitor_inputs, device_id_key):
-        menu = QMenu(title, self)
-        group = QActionGroup(self)
-        group.setExclusive(True)
-
-        for monitor_input in monitor_inputs:
-            action = QAction(str(monitor_input), self)
-            action.setCheckable(True)
-            action.triggered.connect(partial(
-                lambda val: self.user_settings.set(device_id_key, val),
-                val=monitor_input))
-            if monitor_input == self.user_settings.get(device_id_key):
-                action.setChecked(True)
-            group.addAction(action)
-            menu.addAction(action)
-        return menu
-
-    def change_usb_watcher(self, device: Device):
-        self.user_settings.set(USER_SETTINGS_DISPLAY_USB_WATCHER_KEY, device.id)
+    def retrieve_config_panels(self) -> list[ConfigPanel]:
+        from .display_automation_panel import DisplayAutomationConfigPanel
+        return [DisplayAutomationConfigPanel(parent=self)]
 
     @Slot(object, object)
     def device_changed(self, device_notification_type: DeviceNotificationType, usb_device: Device):
