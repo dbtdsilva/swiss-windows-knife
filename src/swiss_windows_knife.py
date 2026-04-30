7460df7eb513d6fdcae6f1ac71199745699c6dfb
Diogo Silva <dbtdsilva@gmail.com>
2026-04-30 14:48:12 +0200

    chore: enable faulthandler at startup so native crashes print a C-stack

--- src/swiss_windows_knife.py
+++ src/swiss_windows_knife.py
@@ -1,4 +1,5 @@
 import ctypes
+import faulthandler
 import inspect
 import logging
 import signal
@@ -33,6 +34,7 @@ class SwissWindowsKnife:
 
     def __init__(self, dev_mode: bool = False) -> None:
         self.init_logging()
+        faulthandler.enable()  # native crashes print a C-stack to stderr
 
         signal.signal(signal.SIGINT, signal.SIG_DFL)
         _set_windows_app_user_model_id()
